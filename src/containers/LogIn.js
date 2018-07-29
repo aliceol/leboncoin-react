34e867bc73dffd776ef5f57bf25e4c25814a361a
Alice Olle-Laprune <aliceol@Alices-MacBook-Pro.local>
2018-07-29 16:02:44 +0200

    login form completed

--- src/containers/LogIn.js
+++ src/containers/LogIn.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "./Login.css";
 
 class LogIn extends React.Component {
   state = {
@@ -47,29 +48,51 @@ class LogIn extends React.Component {
     console.log(this.props.history);
     return (
       <React.Fragment>
-        <form onSubmit={this.onSubmit} className="form form-signup">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            name="email"
-            type="text"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <input type="submit" value="Valider" />
-        </form>
-        <hr />
-        <div>
-          <div>Vous n'avez pas de compte?</div>
-          <Link to="/sign_up">Créer un compte</Link>
+        <div className="container-1024 log_in-box box-shadow f-d-ver a-i-center">
+          <div className="w-50 d-flex f-d-ver a-i-center">
+            <div className="w-80 d-flex f-d-ver">
+              <div className="log_in-header">Connexion</div>
+              <form
+                onSubmit={this.onSubmit}
+                className="form form-login d-flex f-d-ver"
+              >
+                <label className="form-label" htmlFor="email">
+                  Adresse email
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="text"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  className="form-box"
+                />
+                <label className="form-label" htmlFor="email">
+                  Mot de passe
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  className="form-box"
+                />
+                <input
+                  className="validate-login"
+                  type="submit"
+                  value="Se Connecter"
+                />
+              </form>
+            </div>
+            <hr />
+            <div className="w-80 d-flex f-d-ver a-i-center">
+              <div className="no-account">Vous n'avez pas de compte?</div>
+              <div className="link-to-signup w-100 d-flex j-c-center a-i-center">
+                <Link to="/sign_up">Créer un compte</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
